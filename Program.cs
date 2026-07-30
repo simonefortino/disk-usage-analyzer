b272cf7f2be8945a05a4f27e254de88fdac43801
Simone Fortino <simonefortino41@gmail.com>
2026-07-30 10:43:36 +0200

    lettura semplice di file e dimensioni

--- Program.cs
+++ Program.cs
@@ -1,1 +1,52 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+namespace DiskUsageAnalyzer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "/home/simone";
+            long totalFileSizeInBytes = 0;
+            long fileVisitedCount = 0;
+
+            
+
+            try
+            {
+
+                // Se la cartella non esiste lancia un eccezione
+                if(!Directory.Exists(path)) throw new DirectoryNotFoundException();
+
+                Console.WriteLine($"Directory: {path}");
+                
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                var files = dirInfo.EnumerateFiles("*", SearchOption.AllDirectories);
+
+                AnsiConsole.Status().Start(
+                    "Analyzing subdirecotries...",
+                    ctx =>
+                    {
+                        // per ogni file lo conta e somma la dimensione a quella totale
+                        foreach (FileInfo file in files)
+                        {
+                            totalFileSizeInBytes += file.Length; 
+                            fileVisitedCount++;
+                        }
+
+                        AnsiConsole.Markup("[green]Operation completed[/]\n");
+                    }
+                );
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(totalFileSizeInBytes);
+            Console.WriteLine(fileVisitedCount);
+        }
+    }
+}
+
